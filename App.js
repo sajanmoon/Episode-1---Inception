76b2f495e3932d92f3be543f0b582437985cfcf7
sajan moon <sajanmoon5@gmail.com>
2024-01-27 16:05:10 +0530

    buildallfolders

--- App.js
+++ App.js
@@ -1,33 +1,25 @@
-{
-  /* <div id="parent">
-  <div id="child">
-    <h1>I m a H1 tag</h1>
-    <h2>I m a H2 tag</h2>
-  </div>
-  <div id="child">
-    <h1>I m a H1 tag</h1>
-    <h2>I m a H2 tag</h2>
-  </div>
-</div>; */
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I m a h1 tag"),
-    React.createElement("h2", {}, "I m a h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I m a h1 tag"),
-    React.createElement("h2", {}, "I m a h2 tag"),
-  ]),
-]);
+const Title = function () {
+  return (
+    <h1 className="head" tabIndex="5">
+      I m a component using normal function
+    </h1>
+  );
+};
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello world from react"
+const title = <h1>I m a title</h1>;
+
+const Headingcomponent = () => (
+  <div id="container">
+    {/* we can write javascript using curly braces {}  */}
+    <Title />
+    <h1>{title}</h1>
+    <h1 className="heading">I m function component</h1>;
+  </div>
 );
-console.log(heading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<Headingcomponent />);
