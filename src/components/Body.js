bc510137681daf9f61a9751d99e934775d833ffc
sajan moon <sajanmoon5@gmail.com>
2024-01-29 16:52:32 +0530

    setState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,27 @@
 import RestroCard from "./RestroCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRest, setListOfRest] = useState(resList);
+
   return (
     <div className="body">
-      <div className="searchBar">SearchBar</div>
+      <div className="filterBtn">
+        <button
+          className="filter"
+          onClick={() => {
+            const filtrRset = listOfRest.filter(
+              (cards) => cards.data.avgRating > 4
+            );
+            setListOfRest(filtrRset);
+          }}
+        >
+          TOP RATED RESTURANT
+        </button>
+      </div>
       <div className="resContainer">
-        {resList.map((rest) => (
+        {listOfRest.map((rest) => (
           <RestroCard key={rest.data.id} resData={rest} />
         ))}
       </div>
